fe37fc9239640da82081561faaf57068d902f5bf
Vivek <vgdli29@gmail.com>
2022-07-03 17:09:47 +0530

    volunteers added

--- src/components/team/team-home.js
+++ src/components/team/team-home.js
@@ -11,6 +11,21 @@ import teamImage6 from "../../assets/images/team/team-2-6.jpg";
 import teamImage7 from "../../assets/images/team/team-2-7.jpg";
 import yashwantImage from "../../assets/images/team/Yashwant.jpg";
 import vivekImage from "../../assets/images/team/Vivek.jpg";
+import vikashFauzdarImage from "../../assets/images/team/Vikash_Fauzdar.jpg";
+import vikasVermaImage from "../../assets/images/team/Vikas_Verma.jpg";
+import vaishaliMhasaneImage from "../../assets/images/team/Vaishali_Mhasane.jpg";
+import ashishSrivastavaImage from "../../assets/images/team/Ashish_Srivastava.jpg";
+import akshayMittalImage from "../../assets/images/team/Akshay_Mittal.jpg";
+import preetiUpadhyayeImage from "../../assets/images/team/Preeti_Upadhyaye.jpg";
+import prashantPratapSinghImage from "../../assets/images/team/Prashant_Pratap_Singh.jpg";
+import prakashShekhawatImage from "../../assets/images/team/Prakash_shekhawat.jpg";
+import jyotiSinghImage from "../../assets/images/team/Jyoti_Singh.jpg";
+import jitendraKumarImage from "../../assets/images/team/Jitendra_Kumar.jpg";
+import vijayKumarGuptaImage from "../../assets/images/team/Vijay_Kumar_Gupta.jpg";
+import rameshChanderSharmaImage from "../../assets/images/team/Ramesh_Chander_Sharma.jpg";
+import divasShivayPandeImage from "../../assets/images/team/Divas_Shivay_Pande.jpg";
+import hemaSharmaImage from "../../assets/images/team/Hema_Sharma.jpg";
+import aayushiSinghAdhikariImage from "../../assets/images/team/Aayushi_Singh_Adhikari.jpg";
 
 const Directors = [
   {
@@ -59,6 +74,24 @@ const Directors = [
 
 const Volunteers = [
   {
+    image: vijayKumarGuptaImage,
+    extraClassName: "content-bg-1",
+    name: "Mr Vijay Kumar Gupta",
+    designation: "Volunteer"
+  },
+  {
+    image: rameshChanderSharmaImage,
+    extraClassName: "content-bg-1",
+    name: "Mr Ramesh Chander Sharma",
+    designation: "Volunteer"
+  },
+  {
+    image: divasShivayPandeImage,
+    extraClassName: "content-bg-1",
+    name: "Mr Divas Shivay Pande",
+    designation: "Volunteer"
+  },
+  {
     image: vivekImage,
     extraClassName: "content-bg-1",
     name: "Mr Vivek Gupta",
@@ -68,6 +101,78 @@ const Volunteers = [
     image: yashwantImage,
     extraClassName: "content-bg-2",
     name: "Mr. Yashwant K Singh",
+    designation: "Volunteer"
+  },
+  {
+    image: hemaSharmaImage,
+    extraClassName: "content-bg-2",
+    name: "Mr. Hema Sharma",
+    designation: "Volunteer"
+  },
+  {
+    image: aayushiSinghAdhikariImage,
+    extraClassName: "content-bg-2",
+    name: "Mr. Aayushi Singh Adhikari",
+    designation: "Volunteer"
+  },
+  {
+    image: jitendraKumarImage,
+    extraClassName: "content-bg-2",
+    name: "Mr. Jitendra Kumar ",
+    designation: "Volunteer"
+  },
+  {
+    image: jyotiSinghImage,
+    extraClassName: "content-bg-2",
+    name: "Mr. Jyoti Singh",
+    designation: "Volunteer"
+  },
+  {
+    image: prakashShekhawatImage,
+    extraClassName: "content-bg-2",
+    name: "Mr. Prakash Shekhawat",
+    designation: "Volunteer"
+  },
+  {
+    image: prashantPratapSinghImage,
+    extraClassName: "content-bg-2",
+    name: "Mr. Prashant Pratap Singh",
+    designation: "Volunteer"
+  },
+  {
+    image: preetiUpadhyayeImage,
+    extraClassName: "content-bg-2",
+    name: "Mr. Preeti Upadhyaye",
+    designation: "Volunteer"
+  },
+  {
+    image: vikashFauzdarImage,
+    extraClassName: "content-bg-2",
+    name: "Mr. Vikash Fauzdar",
+    designation: "Volunteer"
+  },
+  {
+    image: akshayMittalImage,
+    extraClassName: "content-bg-2",
+    name: "Mr. Akshay Mittal",
+    designation: "Volunteer"
+  },
+  {
+    image: ashishSrivastavaImage,
+    extraClassName: "content-bg-2",
+    name: "Mr. Ashish Srivastava",
+    designation: "Volunteer"
+  },
+  {
+    image: vaishaliMhasaneImage,
+    extraClassName: "content-bg-2",
+    name: "Mr. Vaishali Mhasane",
+    designation: "Volunteer"
+  },
+  {
+    image: vikasVermaImage,
+    extraClassName: "content-bg-2",
+    name: "Mr. Vikas Verma",
     designation: "Volunteer"
   }
 ];
